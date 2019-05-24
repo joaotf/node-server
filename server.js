beda3bd7d22ea196918d10e8fbbe62f262f66bd0
João Tomé <joaotf159@gmail.com>
2019-05-24 13:25:08 -0300

    atualização da porta do servidor

--- server.js
+++ server.js
@@ -1,59 +1,53 @@
-const express = require("express")
-const body = require("body-parser")
+const express = require("express");
+const body = require("body-parser");
 const app = express();
 
-app.use(body.urlencoded({extended: false}))
-app.use(body.json())
+app.use(body.urlencoded({ extended: false }));
+app.use(body.json());
 
-let usuarios = []
+let usuarios = [];
 
+app.get("/usuario", (req, res) => {
+  res.send(usuarios);
+});
 
+app.post("/usuario", (req, res) => {
+  let user = {};
 
-app.get('/usuario',((req,res) => {
-    res.send(usuarios);
-}))
+  user.nome = req.body.nome;
+  user.id = usuarios.length;
 
-app.post('/usuario',((req,res) => {
-    let user = {
+  usuarios.push(user);
+  res.send("Usuário cadastrado com sucesso!");
+});
+
+app.put("/usuario", (req, res) => {
+  try {
+    const { id, nome } = req.body;
+    usuarios[id].nome = nome;
+  } catch (error) {
+    new Error("Index out of Bounds!");
+  }
+
+  res.send("Usuário atualizado com sucesso!");
+});
+
+app.delete("/usuario", (req, res) => {
+  try {
+    const { id } = req.body;
+    arr = [];
+    for (user of usuarios) {
+      if (user.id != id) {
+        arr.push(user);
+      }
     }
+    usuarios = arr;
+  } catch (error) {
+    new Error("Id não encontrado!");
+  }
 
-    user.nome = req.body.nome
-    user.id = usuarios.length
+  res.send("Usuário excluído com sucesso!");
+});
 
-    usuarios.push(user)
-    res.send("Usuário cadastrado com sucesso!")
-}))
-
-app.put('/usuario',((req,res) => {
-    try {
-        const { id,nome } = req.body
-        usuarios[id].nome = nome
-    } catch (error) {
-        new Error("Index out of Bounds!")
-    }
-    
-    res.send("Usuário atualizado com sucesso!")
-}))
-
-app.delete('/usuario',((req,res) => {
-    try {
-        const {id} = req.body
-        arr = []
-        for(user of usuarios){
-            if(user.id != id){
-                arr.push(user)
-            }
-        }
-        usuarios = arr
-    } catch (error) {
-        new Error("Id não encontrado!")
-    }
-    
-    res.send("Usuário excluído com sucesso!")
-}))
-
-
-
-
-
-app.listen(3000)
+const port = process.env.PORT || 3000;
+app.listen(port);
